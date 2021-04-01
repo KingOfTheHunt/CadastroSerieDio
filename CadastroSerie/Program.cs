1eeeffd4fb31105946d5689fcdbfe7f26140eaec
Davi <davifrancisco214@outlook.com>
2021-04-01 00:12:34 -0300

    Modificando o metodo Main

--- CadastroSerie/Program.cs
+++ CadastroSerie/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CadastroSerie.Entidades;
+using System;
 
 namespace CadastroSerie
 {
@@ -6,7 +7,7 @@ namespace CadastroSerie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
