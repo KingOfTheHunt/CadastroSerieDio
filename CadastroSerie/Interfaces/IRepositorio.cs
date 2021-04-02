439817006147059c0b057cb1d1142cce55884905
Davi <davifrancisco214@outlook.com>
2021-04-01 23:24:23 -0300

    Criando a interface que define os metodos do crud

---
+++ CadastroSerie/Interfaces/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CadastroSerie.Interfaces
+{
+    // Defindo os métodos do crud
+    interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T t);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
