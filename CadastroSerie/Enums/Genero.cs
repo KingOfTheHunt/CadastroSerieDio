4c92cba3edcd7e3b5be1cbe98880b930cb06823f
Davi <davifrancisco214@outlook.com>
2021-04-03 19:35:04 -0300

    Adcionado valores personalizados a cada elemento do enum

--- CadastroSerie/Enums/Genero.cs
+++ CadastroSerie/Enums/Genero.cs
@@ -2,18 +2,18 @@
 {
     public enum Genero
     {
-        Acao,
-        Aventura,
-        Comedia,
-        Documentario,
-        Drama,
-        Espionagem,
-        Faroeste,
-        Fantasia,
-        FiccaoCientifica,
-        Musical,
-        Romance,
-        Suspense, 
-        Terror
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        FiccaoCientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12, 
+        Terror = 13
     }
 }
